db75d1e9bd0f5f960686a571ae08d23fd259e53d
dloyolar <dieegolr@gmail.com>
2023-01-25 22:26:23 -0300

    🔧 wip(home): fetch data from service

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import { Banner } from '@/components/Banner';
 import { Header } from '@/components/Header';
 import Head from 'next/head';
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <div>
       <Head>
@@ -13,6 +13,25 @@ export default function Home() {
       <Header />
 
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+        </section>
+
+        {/* Data from service - API */}
+        {data?.map((item, i) => (
+          <h1 key={i}>{item.location}</h1>
+        ))}
+      </main>
     </div>
   );
 }
+
+export const getStaticProps = async (ctx) => {
+  const res = await fetch('https://www.jsonkeeper.com/b/4G1G');
+  const data = await res.json();
+  return {
+    props: { data },
+  };
+};
