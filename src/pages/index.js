fb536c8854bba44e3bc069cf4a1eb8568d42d6c8
dloyolar <dieegolr@gmail.com>
2023-01-26 21:33:15 -0300

    ✨ feat(SmallCard): add component to home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { Banner } from '@/components/Banner';
 import { Header } from '@/components/Header';
+import { SmallCard } from '@/components/SmallCard';
 import Head from 'next/head';
 
 export default function Home({ data }) {
@@ -17,12 +18,18 @@ export default function Home({ data }) {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-        </section>
 
-        {/* Data from service - API */}
-        {data?.map((item, i) => (
-          <h1 key={i}>{item.location}</h1>
-        ))}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {data?.map(({ img, distance, location }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                location={location}
+                distance={distance}
+              />
+            ))}
+          </div>
+        </section>
       </main>
     </div>
   );
