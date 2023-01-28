15c09da8677c6dab1c4976bd2a7a17fde45ae039
dloyolar <dieegolr@gmail.com>
2023-01-28 18:05:27 -0300

    ✨ feat(MediumCard): add new component to home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import { Banner } from '@/components/Banner';
 import { Header } from '@/components/Header';
+import { MediumCard } from '@/components/MediumCard';
 import { SmallCard } from '@/components/SmallCard';
 import Head from 'next/head';
 
-export default function Home({ data }) {
+export default function Home({ data, cardsData }) {
   return (
     <div>
       <Head>
@@ -30,15 +31,31 @@ export default function Home({ data }) {
             ))}
           </div>
         </section>
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
+        </section>
       </main>
     </div>
   );
 }
 
 export const getStaticProps = async (ctx) => {
-  const res = await fetch('https://www.jsonkeeper.com/b/4G1G');
-  const data = await res.json();
+  const data = await fetch('https://www.jsonkeeper.com/b/4G1G').then((res) =>
+    res.json()
+  );
+
+  const cardsData = await fetch('https://www.jsonkeeper.com/b/VHHT').then(
+    (res) => res.json()
+  );
+
   return {
-    props: { data },
+    props: { data, cardsData },
   };
 };
