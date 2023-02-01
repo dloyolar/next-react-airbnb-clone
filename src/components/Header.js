43aebaecdb5d3788e382c5b0470596d456757822
dloyolar <dieegolr@gmail.com>
2023-02-01 17:50:53 -0300

    ✨ feat(Header): add calendar picker component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,33 @@ import {
   GlobeAltIcon,
   Bars4Icon,
 } from '@heroicons/react/24/outline';
+import { UsersIcon } from '@heroicons/react/24/solid';
+import { useState } from 'react';
+import { DateRangePicker } from 'react-date-range';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 
 export const Header = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuests, setNoOfGuests] = useState(1);
+
+  const resetInput = () => {
+    setSearchInput('');
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const selectionRange = {
+    startDate,
+    endDate,
+    key: 'selection',
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-5">
       {/* left */}
@@ -22,6 +47,8 @@ export const Header = () => {
       {/*  middle */}
       <div className="flex items-center border-2 rounded-full py-2 md:shadow-sm">
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           className="flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400"
           type="text"
           placeholder="Start your search"
@@ -39,6 +66,38 @@ export const Header = () => {
           <UserCircleIcon className="h-6 cursor-pointer" />
         </div>
       </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            minDate={new Date()}
+            ranges={[selectionRange]}
+            rangeColors={['#FD5B61']}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+
+            <UsersIcon className="h-5" />
+            <input
+              value={noOfGuests}
+              onChange={(e) => setNoOfGuests(e.target.value)}
+              type="number"
+              className="w-12 pl-2 outline-none text-red-400"
+              min={1}
+            />
+          </div>
+
+          <div className="flex">
+            <button onClick={resetInput} className="flex-grow text-gray-500">
+              Cancel
+            </button>
+            <button className="flex-grow text-red-400">Search</button>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
