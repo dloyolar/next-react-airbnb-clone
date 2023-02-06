585a1aed980f96d4e0b38582a706f3af7378e8e2
dloyolar <dieegolr@gmail.com>
2023-02-05 22:46:44 -0300

    ✨ feat(InfoCard): finish component

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import { StarIcon } from '@heroicons/react/24/solid';
+import { HeartIcon } from '@heroicons/react/24/outline';
 
 export const InfoCard = ({
   img,
@@ -10,14 +12,39 @@ export const InfoCard = ({
   total,
 }) => {
   return (
-    <div>
-      <div className="relative h-24 w-40 md:h-52 md:w-80  flex-shrink">
+    <div className="flex py-7 px-2 border-b pr-4 cursor-pointer hover:opacity-80 hover:shadow-lg transition duration-200 ease-out first:border-t">
+      <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink">
         <Image
           src={img}
           fill
           style={{ objectFit: 'cover' }}
           alt={`${title} image`}
         />
+      </div>
+
+      <div className="flex flex-col flex-grow pl-5">
+        <div className="flex justify-between">
+          <p>{location}</p>
+          <HeartIcon className="h-7 cursor-pointer" />
+        </div>
+
+        <h4 className="text-xl">{title}</h4>
+
+        <div className="border-b w-10 pt-2" />
+
+        <p className="pt-2 text-sm text-gray-500 flex-grow">{description}</p>
+
+        <div className="flex justify-between items-end pt-5">
+          <p className="flex items-center">
+            <StarIcon className="h-5 text-red-400" />
+            {star}
+          </p>
+
+          <div>
+            <p className="text-lg lg:text-2xl font-semibold pb-2">{price}</p>
+            <p className="text-right font-extralight">{total}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
